e29856f75e5b2690bf211692c16807a5a5692e6f
Chaudhary Sumit <86030135+chaudharysumit001@users.noreply.github.com>
2024-01-03 05:03:44 +0000

    updates

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,12 @@
+// console.log(2>1);
+
+// console.log("2"+1);
+// console.log("2"< 1);
+
+const id = Symbol(12333);
+const id2 = Symbol(12333);
+console.log(id == id2);
+
+let biginfffft = 284892853780393434n;
+
+console.log(typeof biginfffft);
