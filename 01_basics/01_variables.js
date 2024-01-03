82ac898f8f03645da2e965e07041346df6e154b9
Chaudhary Sumit <86030135+chaudharysumit001@users.noreply.github.com>
2024-01-03 03:33:28 +0000

    update

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553;
+let accountEmail = "sumit@mail.com";
+var accountPass = "12345";
+accountCity = "faridabad";
+
+// accountId = 2;
+console.log(accountId);
+accountEmail = "st@gmail.com"
+accountPass = "11323";
+accountCity = "palwal";
+
+console.table([accountId, accountPass, accountEmail, accountCity])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,7 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("hello") // using node js it will not work, not browser
+
+let str = "hello";
+let str2 = "sumit";
+console.log(str + str2);
